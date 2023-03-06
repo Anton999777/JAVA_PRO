9f4fb98754d8ab17a7d873de0d23220b51213a02
Shevchenko Anton <anton.shevchenko999777@gmail.com>
2023-03-06 14:25:32 +0100

    create new directory, and Added HomeWork6

---
+++ HomeWork/src/main/java/HomeWork6/Car.java
@@ -0,0 +1,16 @@
+package HomeWork6;
+
+public class Car {
+    private void startElectricity () {
+    }
+    private void startCommand () {
+    }
+    private void startFuelSystem () {
+    }
+
+    public void start () {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+}
