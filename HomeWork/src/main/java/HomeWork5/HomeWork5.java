d9a1b208abf90821419cc47015b2e0d98936a04f
Shevchenko Anton <anton.shevchenko999777@gmail.com>
2023-03-15 17:56:00 +0100

    formated code

--- HomeWork/src/main/java/HomeWork5/HomeWork5.java
+++ HomeWork/src/main/java/HomeWork5/HomeWork5.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWork5 {
-    final static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado" , "broccoli", "carrot",
+    final static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
             "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", " pea", "peanut",
             "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
@@ -17,7 +17,8 @@ public class HomeWork5 {
         System.out.println(isPalindrome(null));
         guessTheWordGame();
     }
-    public static void guessTheWordGame () {
+
+    public static void guessTheWordGame() {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
@@ -35,7 +36,7 @@ public class HomeWork5 {
                 StringBuilder str = new StringBuilder();
                 for (int i = 0; i < words.length; i++) {
 
-                    if (i == inputUser.length() || i == wordComputer.length()){
+                    if (i == inputUser.length() || i == wordComputer.length()) {
                         str.append("############");
                         break;
                     }
@@ -50,7 +51,7 @@ public class HomeWork5 {
         }
     }
 
-    public static String stringReverse (String source) {
+    public static String stringReverse(String source) {
         if (source == null) {
             throw new NullPointerException("Error: you didn't enter any data." + "\n" +
                     "null was passed to the argument 'source'.");
@@ -58,37 +59,39 @@ public class HomeWork5 {
         return new StringBuilder(source).reverse().toString();
     }
 
-    public static int findSymbolOccurance (String name, char symbol) {
+    public static int findSymbolOccurance(String name, char symbol) {
+        Objects.requireNonNull(name, "Error: you didn't enter any data." + "\n" +
+                "Null was passed to the argument 'name'.");
+
         int counter = 0;
-            Objects.requireNonNull(name, "Error: you didn't enter any data." + "\n" +
-                    "Null was passed to the argument 'name'.");
 
-            char[] symbols = name.toCharArray();
+        char[] symbols = name.toCharArray();
 
-            for (int i = 0; i < symbols.length; i++) {
-                if (symbol == symbols[i]) {
-                    counter++;
-                }
+        for (int i = 0; i < symbols.length; i++) {
+            if (symbol == symbols[i]) {
+                counter++;
             }
+        }
         return counter;
     }
 
-    public static int findWordPosition (String source, String target) {
+    public static int findWordPosition(String source, String target) {
         if (source == null || target == null) {
             throw new NullPointerException("Error: you didn't enter any data." + "\n" +
                     "Null was passed to the argument. class HomeWork5/ line_84");
         }
-            return source.indexOf(target);
+        return source.indexOf(target);
     }
-    public static boolean isPalindrome (String palindrome) {
+
+    public static boolean isPalindrome(String palindrome) {
         if (palindrome == null) {
             System.out.println("Error: you didn't enter any data." +
                     "\n" +
                     "null was passed to the argument 'palindrome'.");
             System.exit(1);
         }
-            String str = new StringBuilder(palindrome).reverse().toString();
-            return str.equals(palindrome);
-        }
+        String str = new StringBuilder(palindrome).reverse().toString();
+        return str.equals(palindrome);
     }
+}
 
