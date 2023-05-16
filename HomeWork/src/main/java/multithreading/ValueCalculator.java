6290790c092b104e2f99ae1ceebd1cab05206d8e
Shevchenko Anton <anton.shevchenko999777@gmail.com>
2023-05-16 11:54:43 +0200

    added method result

--- HomeWork/src/main/java/multithreading/ValueCalculator.java
+++ HomeWork/src/main/java/multithreading/ValueCalculator.java
@@ -11,21 +11,21 @@ public class ValueCalculator implements Runnable {
     private volatile long start;
     private volatile long finish;
 
-    private long currentTimeMillis() {
+    public long currentTimeMillis() {
         return System.currentTimeMillis();
     }
 
-    private int[] arrayFilling(int[] arr) {
+    public int[] arrayFilling(int[] arr) {
         Arrays.fill(arr, 3);
         return arr;
     }
 
-    private void splittingArray() {
+    public void splittingArray() {
         System.arraycopy(numbers, 0, numbers, 0, half);
         System.arraycopy(numbers, half, secondHalfNumbers, 0, half);
     }
 
-    private float[] convertToFloat(int[] arr) {
+    public float[] convertToFloat(int[] arr) {
         float[] floats = new float[arr.length];
         for (int i = 0; i < arr.length; i++) {
             floats[i] = arr[i];
@@ -33,18 +33,23 @@ public class ValueCalculator implements Runnable {
         return floats;
     }
 
-    private float[] newValueForArray(float[] arr) {
+    public float[] newValueForArray(float[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         return arr;
     }
 
-    private float[] joiningArrays(float[] arr1, float[] arr2) {
+    public float[] joiningArrays(float[] arr1, float[] arr2) {
         float[] floats = new float[lengthArrayNum];
         System.arraycopy(arr1, 0, floats, 0, half);
         System.arraycopy(arr2, 0, floats, half, half);
         return floats;
+    }
+
+    public String result () {
+        return "Current" + Thread.currentThread().getName()
+                + ": Elapsed time from the start of the program to its completion : " + (finish - start);
     }
 
     @Override
@@ -56,7 +61,6 @@ public class ValueCalculator implements Runnable {
         joiningArrays(convertToFloat(secondHalfNumbers), newValueForArray(convertToFloat(numbers)));
         finish = currentTimeMillis();
 
-        System.out.println("Current" + Thread.currentThread().getName()
-                + ": Elapsed time from the start of the program to its completion : " + (finish - start));
+        System.out.println(result());
     }
 }
